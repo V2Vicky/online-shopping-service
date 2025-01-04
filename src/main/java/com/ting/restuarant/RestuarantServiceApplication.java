07bb3e994cc9261bfa95ab168e19401f8ff12cb8
V2Vicky <v2vigneshtech111@gmail.com>
2025-01-04 20:53:14 +0530

    first commit

---
+++ src/main/java/com/ting/restuarant/RestuarantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ting.restuarant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestuarantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestuarantServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ting/restuarant/RestuarantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ting.restuarant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestuarantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
